71fa82b69e57404ca1075e6c14caace5bf49254b
Winnifried Wollner <wollner@mathematik.tu-darmstadt.de>
2012-05-04 12:13:54 +0000

    Removed the template instantiations. This should make compiling the library much faster.

--- DOpEsrc/source/multimesh_celldatacontainer.cc
+++
@@ -1,8 +0,0 @@
-#include "multimesh_celldatacontainer.h"
-
-//Explicit instantiation
-template class DOpE::Multimesh_CellDataContainer<dealii::DoFHandler<deal_II_dimension>,
-    dealii::Vector<double>, deal_II_dimension>;
-template class DOpE::Multimesh_CellDataContainer<dealii::DoFHandler<deal_II_dimension>,
-    dealii::BlockVector<double>, deal_II_dimension>;
-
