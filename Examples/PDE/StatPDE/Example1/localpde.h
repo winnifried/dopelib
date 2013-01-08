ad4dae08db38850a07a4378f657076b1deb370d8
Christian Goll <christian.goll@iwr.uni-heidelberg.de>
2013-01-08 09:40:52 +0000

    Added the possibility to use hp in this example. The user has just to switch a few lines of code in main.cc

--- Examples/PDE/StatPDE/Example1/localpde.h
+++ Examples/PDE/StatPDE/Example1/localpde.h
@@ -1,25 +1,25 @@
 /**
-*
-* Copyright (C) 2012 by the DOpElib authors
-*
-* This file is part of DOpElib
-*
-* DOpElib is free software: you can redistribute it
-* and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either
-* version 3 of the License, or (at your option) any later
-* version.
-*
-* DOpElib is distributed in the hope that it will be
-* useful, but WITHOUT ANY WARRANTY; without even the implied
-* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-* PURPOSE.  See the GNU General Public License for more
-* details.
-*
-* Please refer to the file LICENSE.TXT included in this distribution
-* for further information on this license.
-*
-**/
+ *
+ * Copyright (C) 2012 by the DOpElib authors
+ *
+ * This file is part of DOpElib
+ *
+ * DOpElib is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * DOpElib is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * Please refer to the file LICENSE.TXT included in this distribution
+ * for further information on this license.
+ *
+ **/
 
 #ifndef _LOCALPDE_
 #define _LOCALPDE_
@@ -32,40 +32,41 @@ using namespace std;
 using namespace dealii;
 using namespace DOpE;
 
-template<typename VECTOR, int dealdim>
-  class LocalPDE : public PDEInterface<CellDataContainer,FaceDataContainer,dealii::DoFHandler<dealdim>, VECTOR, dealdim>
+template<typename DOFHANDLER, typename VECTOR, int dealdim>
+  class LocalPDE : public PDEInterface<CellDataContainer, FaceDataContainer,
+      DOFHANDLER, VECTOR, dealdim>
   {
-  public:
-    LocalPDE() :
-      _state_block_components(3, 0)
-    {
-      _state_block_components[2] = 1;
-    }
+    public:
+      LocalPDE()
+          : _state_block_components(3, 0)
+      {
+        _state_block_components[2] = 1;
+      }
 
-    // Domain values for cells
-    void
-    CellEquation(
-        const CellDataContainer<dealii::DoFHandler<dealdim>, VECTOR, dealdim>& cdc,
-        dealii::Vector<double> &local_cell_vector, double scale, double /*scale_ico*/)
-    {
-      const DOpEWrapper::FEValues<dealdim> & state_fe_values =
-          cdc.GetFEValuesState();
-      unsigned int n_dofs_per_cell = cdc.GetNDoFsPerCell();
-      unsigned int n_q_points = cdc.GetNQPoints();
-      assert(this->_problem_type == "state");
+      // Domain values for cells
+      void
+      CellEquation(const CellDataContainer<DOFHANDLER, VECTOR, dealdim>& cdc,
+      dealii::Vector<double> &local_cell_vector, double scale
+          , double /*scale_ico*/)
+      {
+        const DOpEWrapper::FEValues<dealdim> & state_fe_values =
+            cdc.GetFEValuesState();
+        unsigned int n_dofs_per_cell = cdc.GetNDoFsPerCell();
+        unsigned int n_q_points = cdc.GetNQPoints();
+        assert(this->_problem_type == "state");
 
-      _uvalues.resize(n_q_points, Vector<double> (3));
-      _ugrads.resize(n_q_points, vector<Tensor<1, 2> > (3));
+        _uvalues.resize(n_q_points, Vector<double>(3));
+        _ugrads.resize(n_q_points, vector < Tensor<1, 2> > (3));
 
-      cdc.GetValuesState("last_newton_solution", _uvalues);
-      cdc.GetGradsState("last_newton_solution", _ugrads);
+        cdc.GetValuesState("last_newton_solution", _uvalues);
+        cdc.GetGradsState("last_newton_solution", _ugrads);
 
-      const FEValuesExtractors::Vector velocities(0);
-      const FEValuesExtractors::Scalar pressure(2);
+        const FEValuesExtractors::Vector velocities(0);
+        const FEValuesExtractors::Scalar pressure(2);
 
-      for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
+        for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
         {
-          Tensor<2, 2> vgrads;
+          Tensor < 2, 2 > vgrads;
           vgrads.clear();
           vgrads[0][0] = _ugrads[q_point][0][0];
           vgrads[0][1] = _ugrads[q_point][0][1];
@@ -76,217 +77,219 @@ template<typename VECTOR, int dealdim>
           double incompressibility = vgrads[0][0] + vgrads[1][1];
 
           for (unsigned int i = 0; i < n_dofs_per_cell; i++)
-            {
+          {
 //              const Tensor<1, 2> phi_i_v = state_fe_values[velocities].value(i,
 //                  q_point);
-              const Tensor<2, 2> phi_i_grads_v =
-                  state_fe_values[velocities].gradient(i, q_point);
-              const double phi_i_p =
-                  state_fe_values[pressure].value(i, q_point);
-              const double div_phi_v = state_fe_values[velocities].divergence(
-                  i, q_point);
+            const Tensor<2, 2> phi_i_grads_v =
+                state_fe_values[velocities].gradient(i, q_point);
+            const double phi_i_p = state_fe_values[pressure].value(i, q_point);
+            const double div_phi_v = state_fe_values[velocities].divergence(i,
+                q_point);
 
-              local_cell_vector(i) += scale * (0.5 * scalar_product(vgrads,
-                  phi_i_grads_v) + 0.5 * scalar_product(transpose(vgrads),
-                  phi_i_grads_v) - press * div_phi_v + incompressibility
-                  * phi_i_p) * state_fe_values.JxW(q_point);
+            local_cell_vector(i) += scale
+                * (0.5 * scalar_product(vgrads, phi_i_grads_v)
+                    + 0.5 * scalar_product(transpose(vgrads), phi_i_grads_v)
+                    - press * div_phi_v + incompressibility * phi_i_p)
+                * state_fe_values.JxW(q_point);
 
-            }
+          }
         }
 
-    }
+      }
 
-    void
-    CellMatrix(
-        const CellDataContainer<dealii::DoFHandler<dealdim>, VECTOR, dealdim>& cdc,
-        FullMatrix<double> &local_entry_matrix,
-        double scale, double /*scale_ico*/)
-    {
+      void
+      CellMatrix(const CellDataContainer<DOFHANDLER, VECTOR, dealdim>& cdc,
+      FullMatrix<double> &local_entry_matrix,
+      double scale, double /*scale_ico*/)
+      {
 
-      const DOpEWrapper::FEValues<dealdim> & state_fe_values =
-          cdc.GetFEValuesState();
-      unsigned int n_dofs_per_cell = cdc.GetNDoFsPerCell();
-      unsigned int n_q_points = cdc.GetNQPoints();
+        const DOpEWrapper::FEValues<dealdim> & state_fe_values =
+            cdc.GetFEValuesState();
+        unsigned int n_dofs_per_cell = cdc.GetNDoFsPerCell();
+        unsigned int n_q_points = cdc.GetNQPoints();
 
-      const FEValuesExtractors::Vector velocities(0);
-      const FEValuesExtractors::Scalar pressure(2);
+        const FEValuesExtractors::Vector velocities(0);
+        const FEValuesExtractors::Scalar pressure(2);
 
-      std::vector<Tensor<1, 2> > phi_v(n_dofs_per_cell);
-      std::vector<Tensor<2, 2> > phi_grads_v(n_dofs_per_cell);
-      std::vector<double> phi_p(n_dofs_per_cell);
-      std::vector<double> div_phi_v(n_dofs_per_cell);
+        std::vector < Tensor<1, 2> > phi_v(n_dofs_per_cell);
+        std::vector < Tensor<2, 2> > phi_grads_v(n_dofs_per_cell);
+        std::vector<double> phi_p(n_dofs_per_cell);
+        std::vector<double> div_phi_v(n_dofs_per_cell);
 
-      for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
+        for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
         {
           for (unsigned int k = 0; k < n_dofs_per_cell; k++)
-            {
-              phi_v[k] = state_fe_values[velocities].value(k, q_point);
-              phi_grads_v[k] = state_fe_values[velocities].gradient(k, q_point);
-              phi_p[k] = state_fe_values[pressure].value(k, q_point);
-              div_phi_v[k] = state_fe_values[velocities].divergence(k, q_point);
-            }
+          {
+            phi_v[k] = state_fe_values[velocities].value(k, q_point);
+            phi_grads_v[k] = state_fe_values[velocities].gradient(k, q_point);
+            phi_p[k] = state_fe_values[pressure].value(k, q_point);
+            div_phi_v[k] = state_fe_values[velocities].divergence(k, q_point);
+          }
 
           for (unsigned int i = 0; i < n_dofs_per_cell; i++)
+          {
+            for (unsigned int j = 0; j < n_dofs_per_cell; j++)
             {
-              for (unsigned int j = 0; j < n_dofs_per_cell; j++)
-                {
-                  //const Tensor<2,2> phi_j_grads_v = state_fe_values[velocities].gradient (j, q_point);
-                  //const double phi_j_p = state_fe_values[pressure].value (j, q_point);
+              //const Tensor<2,2> phi_j_grads_v = state_fe_values[velocities].gradient (j, q_point);
+              //const double phi_j_p = state_fe_values[pressure].value (j, q_point);
 
-                  local_entry_matrix(i, j) += scale * (0.5 * scalar_product(
-                      phi_grads_v[j], phi_grads_v[i]) + 0.5 * scalar_product(
-                      transpose(phi_grads_v[j]), phi_grads_v[i]) - phi_p[j]
-                      * div_phi_v[i] + (phi_grads_v[j][0][0]
-                      + phi_grads_v[j][1][1]) * phi_p[i])
-                      * state_fe_values.JxW(q_point);
-                }
+              local_entry_matrix(i, j) +=
+                  scale
+                      * (0.5 * scalar_product(phi_grads_v[j], phi_grads_v[i])
+                          + 0.5
+                              * scalar_product(transpose(phi_grads_v[j]),
+                                  phi_grads_v[i]) - phi_p[j] * div_phi_v[i]
+                          + (phi_grads_v[j][0][0] + phi_grads_v[j][1][1])
+                              * phi_p[i]) * state_fe_values.JxW(q_point);
             }
+          }
         }
 
-    }
+      }
 
-    void
-    CellRightHandSide(
-        const CellDataContainer<dealii::DoFHandler<dealdim>, VECTOR, dealdim>& /*cdc*/,
-        dealii::Vector<double> &local_cell_vector __attribute__((unused)), double scale __attribute__((unused)))
-    {
-      
-    }
+      void
+      CellRightHandSide(const CellDataContainer<DOFHANDLER, VECTOR, dealdim>&
+          /*cdc*/,
+          dealii::Vector<double> &local_cell_vector
+          __attribute__((unused)), double scale __attribute__((unused)))
+      {
 
-    // Values for boundary integrals
-    void
-    BoundaryEquation(
-        const FaceDataContainer<dealii::DoFHandler<2>, VECTOR, dealdim>& fdc,
-        dealii::Vector<double> &local_cell_vector, double scale, double /*scale_ico*/)
-    {
-      const auto & state_fe_face_values =
-          fdc.GetFEFaceValuesState();
-      unsigned int n_dofs_per_cell = fdc.GetNDoFsPerCell();
-      unsigned int n_q_points = fdc.GetNQPoints();
-      unsigned int color = fdc.GetBoundaryIndicator();
+      }
 
-      assert(this->_problem_type == "state");
+      // Values for boundary integrals
+      void
+      BoundaryEquation(
+          const FaceDataContainer<DOFHANDLER, VECTOR, dealdim>& fdc,
+          dealii::Vector<double> &local_cell_vector, double scale
+          , double /*scale_ico*/)
+      {
+        const auto & state_fe_face_values = fdc.GetFEFaceValuesState();
+        unsigned int n_dofs_per_cell = fdc.GetNDoFsPerCell();
+        unsigned int n_q_points = fdc.GetNQPoints();
+        unsigned int color = fdc.GetBoundaryIndicator();
 
-      // do-nothing applied on outflow boundary
-      if (color == 1)
+        assert(this->_problem_type == "state");
+
+        // do-nothing applied on outflow boundary
+        if (color == 1)
         {
-          _ufacegrads.resize(n_q_points, vector<Tensor<1, 2> > (3));
+          _ufacegrads.resize(n_q_points, vector < Tensor<1, 2> > (3));
 
           fdc.GetFaceGradsState("last_newton_solution", _ufacegrads);
 
           const FEValuesExtractors::Vector velocities(0);
 
           for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
+          {
+            Tensor < 2, 2 > vgrads;
+            vgrads.clear();
+            vgrads[0][0] = _ufacegrads[q_point][0][0];
+            vgrads[0][1] = _ufacegrads[q_point][0][1];
+            vgrads[1][0] = _ufacegrads[q_point][1][0];
+            vgrads[1][1] = _ufacegrads[q_point][1][1];
+
+            for (unsigned int i = 0; i < n_dofs_per_cell; i++)
             {
-              Tensor<2, 2> vgrads;
-              vgrads.clear();
-              vgrads[0][0] = _ufacegrads[q_point][0][0];
-              vgrads[0][1] = _ufacegrads[q_point][0][1];
-              vgrads[1][0] = _ufacegrads[q_point][1][0];
-              vgrads[1][1] = _ufacegrads[q_point][1][1];
+              const Tensor<1, 2> phi_i_v =
+                  state_fe_face_values[velocities].value(i, q_point);
 
-              for (unsigned int i = 0; i < n_dofs_per_cell; i++)
-                {
-                  const Tensor<1, 2> phi_i_v =
-                      state_fe_face_values[velocities].value(i, q_point);
+              const Tensor<1, 2> neumann_value = (transpose(vgrads)
+                  * state_fe_face_values.normal_vector(q_point));
 
-                  const Tensor<1, 2> neumann_value = (transpose(vgrads)
-                      * state_fe_face_values.normal_vector(q_point));
-
-                  local_cell_vector(i) += -scale * 0.5 * neumann_value
-                      * phi_i_v * state_fe_face_values.JxW(q_point);
-                }
+              local_cell_vector(i) += -scale * 0.5 * neumann_value * phi_i_v
+                  * state_fe_face_values.JxW(q_point);
             }
+          }
         }
 
-    }
+      }
 
-    void
-    BoundaryMatrix(
-        const FaceDataContainer<dealii::DoFHandler<2>, VECTOR, dealdim>& fdc,
-        dealii::FullMatrix<double> &local_entry_matrix, double /*scale*/, double /*scale_ico*/)
-    {
-      const auto & state_fe_face_values =
-          fdc.GetFEFaceValuesState();
-      unsigned int n_dofs_per_cell = fdc.GetNDoFsPerCell();
-      unsigned int n_q_points = fdc.GetNQPoints();
-      unsigned int color = fdc.GetBoundaryIndicator();
-      assert(this->_problem_type == "state");
+      void
+      BoundaryMatrix(
+          const FaceDataContainer<DOFHANDLER, VECTOR, dealdim>& fdc,
+          dealii::FullMatrix<double> &local_entry_matrix, double
+          /*scale*/, double /*scale_ico*/)
+      {
+        const auto & state_fe_face_values = fdc.GetFEFaceValuesState();
+        unsigned int n_dofs_per_cell = fdc.GetNDoFsPerCell();
+        unsigned int n_q_points = fdc.GetNQPoints();
+        unsigned int color = fdc.GetBoundaryIndicator();
+        assert(this->_problem_type == "state");
 
-      // do-nothing applied on outflow boundary
-      if (color == 1)
+        // do-nothing applied on outflow boundary
+        if (color == 1)
         {
           const FEValuesExtractors::Vector velocities(0);
 
           for (unsigned int q_point = 0; q_point < n_q_points; q_point++)
+          {
+            for (unsigned int i = 0; i < n_dofs_per_cell; i++)
             {
-              for (unsigned int i = 0; i < n_dofs_per_cell; i++)
-                {
-                  const Tensor<1, 2> phi_i_v =
-                      state_fe_face_values[velocities].value(i, q_point);
+              const Tensor<1, 2> phi_i_v =
+                  state_fe_face_values[velocities].value(i, q_point);
 
-                  for (unsigned int j = 0; j < n_dofs_per_cell; j++)
-                    {
-                      const Tensor<2, 2> phi_j_grads_v =
-                          state_fe_face_values[velocities].gradient(j, q_point);
+              for (unsigned int j = 0; j < n_dofs_per_cell; j++)
+              {
+                const Tensor<2, 2> phi_j_grads_v =
+                    state_fe_face_values[velocities].gradient(j, q_point);
 
-                      const Tensor<1, 2> neumann_value = (transpose(
-                          phi_j_grads_v) * state_fe_face_values.normal_vector(
-                          q_point));
+                const Tensor<1, 2> neumann_value = (transpose(phi_j_grads_v)
+                    * state_fe_face_values.normal_vector(q_point));
 
-                      local_entry_matrix(i, j) += -0.5 * neumann_value
-                          * phi_i_v * state_fe_face_values.JxW(q_point);
-                    }
-                }
+                local_entry_matrix(i, j) += -0.5 * neumann_value * phi_i_v
+                    * state_fe_face_values.JxW(q_point);
+              }
             }
+          }
         }
 
-    }
+      }
 
-    void
-    BoundaryRightHandSide(
-        const FaceDataContainer<dealii::DoFHandler<2>, VECTOR, dealdim>&,
-        dealii::Vector<double> &local_cell_vector __attribute__((unused)), double scale __attribute__((unused)))
-    {
-    }
+      void
+      BoundaryRightHandSide(
+          const FaceDataContainer<DOFHANDLER, VECTOR, dealdim>&,
+          dealii::Vector<double> &local_cell_vector
+          __attribute__((unused)), double scale __attribute__((unused)))
+      {
+      }
 
-    UpdateFlags
-    GetUpdateFlags() const
-    {
-      return update_values | update_gradients | update_quadrature_points;
-    }
+      UpdateFlags
+      GetUpdateFlags() const
+      {
+        return update_values | update_gradients | update_quadrature_points;
+      }
 
-    UpdateFlags
-    GetFaceUpdateFlags() const
-    {
-      return update_values | update_gradients | update_normal_vectors
+      UpdateFlags
+      GetFaceUpdateFlags() const
+      {
+        return update_values | update_gradients | update_normal_vectors
             | update_quadrature_points;
-    }
+      }
 
-    unsigned int
-    GetStateNBlocks() const
-    {
-      return 2;
-    }
-    std::vector<unsigned int>&
-    GetStateBlockComponent()
-    {
-      return _state_block_components;
-    }
-    const std::vector<unsigned int>&
-    GetStateBlockComponent() const
-    {
-      return _state_block_components;
-    }
+      unsigned int
+      GetStateNBlocks() const
+      {
+        return 2;
+      }
+      std::vector<unsigned int>&
+      GetStateBlockComponent()
+      {
+        return _state_block_components;
+      }
+      const std::vector<unsigned int>&
+      GetStateBlockComponent() const
+      {
+        return _state_block_components;
+      }
 
-  private:
-    vector<Vector<double> > _uvalues;
+    private:
+      vector<Vector<double> > _uvalues;
 
-    vector<vector<Tensor<1, dealdim> > > _ugrads;
+      vector<vector<Tensor<1, dealdim> > > _ugrads;
 
-    // face values
-    vector<vector<Tensor<1, dealdim> > > _ufacegrads;
+      // face values
+      vector<vector<Tensor<1, dealdim> > > _ufacegrads;
 
-    vector<unsigned int> _state_block_components;
+      vector<unsigned int> _state_block_components;
   };
 #endif
--- Examples/PDE/StatPDE/Example1/main.cc
+++ Examples/PDE/StatPDE/Example1/main.cc
@@ -1,25 +1,25 @@
 /**
-*
-* Copyright (C) 2012 by the DOpElib authors
-*
-* This file is part of DOpElib
-*
-* DOpElib is free software: you can redistribute it
-* and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either
-* version 3 of the License, or (at your option) any later
-* version.
-*
-* DOpElib is distributed in the hope that it will be
-* useful, but WITHOUT ANY WARRANTY; without even the implied
-* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-* PURPOSE.  See the GNU General Public License for more
-* details.
-*
-* Please refer to the file LICENSE.TXT included in this distribution
-* for further information on this license.
-*
-**/
+ *
+ * Copyright (C) 2012 by the DOpElib authors
+ *
+ * This file is part of DOpElib
+ *
+ * DOpElib is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * DOpElib is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * Please refer to the file LICENSE.TXT included in this distribution
+ * for further information on this license.
+ *
+ **/
 
 #include "pdeproblemcontainer.h"
 #include "functionalinterface.h"
@@ -51,7 +51,7 @@
 
 #include "localpde.h"
 #include "functionals.h"
-
+#include "indexsetter.h"
 #include "my_functions.h"
 
 using namespace std;
@@ -61,14 +61,22 @@ using namespace DOpE;
 #define MATRIX BlockSparseMatrix<double>
 #define SPARSITYPATTERN BlockSparsityPattern
 #define VECTOR BlockVector<double>
-#define DOFHANDLER DoFHandler<2>
-#define FE FESystem<2>
 
-//typedef PDEProblemContainer<PDEInterface<DOFHANDLER,VECTOR, 2>,DirichletDataInterface<VECTOR,2>,SPARSITYPATTERN, VECTOR,2> OP;
-typedef PDEProblemContainer<LocalPDE<VECTOR, 2>,
-			    DirichletDataInterface<VECTOR, 2>, SPARSITYPATTERN, VECTOR, 2> OP;
-typedef IntegratorDataContainer<DOFHANDLER, Quadrature<2>,
-   Quadrature<1>, VECTOR, 2> IDC;
+#define DOFHANDLER DoFHandler<2>
+#define FE FESystem<2, 2>
+#define QUADRATURE Quadrature<2>
+#define FACEQUADRATURE Quadrature<1>
+//**********hp*************************
+//#define DOFHANDLER hp::DoFHandler<2>
+//#define FE hp::FECollection<2, 2>
+//#define QUADRATURE hp::QCollection<2>
+//#define FACEQUADRATURE hp::QCollection<1>
+//**********hp*************************
+
+typedef PDEProblemContainer<LocalPDE<DOFHANDLER, VECTOR, 2> ,
+DirichletDataInterface<VECTOR, 2> ,SPARSITYPATTERN,VECTOR, 2,FE,DOFHANDLER> OP;
+typedef IntegratorDataContainer<DOFHANDLER, QUADRATURE, FACEQUADRATURE, VECTOR,
+    2> IDC;
 
 typedef Integrator<IDC, VECTOR, double, 2> INTEGRATOR;
 
@@ -84,20 +92,21 @@ main(int argc, char **argv)
    *  In this example we solve stationary (linear) Stokes' equations
    *  with symmetric stress tensor and do-nothing condition on
    *  the outflow boundary. In this case we employ an additional
-   *  term on the outlfow boundary due the symmetry of the stress tensor
+   *  term on the outflow boundary due the symmetry of the stress tensor.
+   *
+   *  To make this run with hp, just replace the indicated parts.
    */
-  //TODO Make this run with hp
   string paramfile = "dope.prm";
 
   if (argc == 2)
-    {
-      paramfile = argv[1];
-    }
+  {
+    paramfile = argv[1];
+  }
   else if (argc > 2)
-    {
-      std::cout << "Usage: " << argv[0] << " [ paramfile ] " << std::endl;
-      return -1;
-    }
+  {
+    std::cout << "Usage: " << argv[0] << " [ paramfile ] " << std::endl;
+    return -1;
+  }
 
   ParameterReader pr;
   SSolver::declare_params(pr);
@@ -112,35 +121,40 @@ main(int argc, char **argv)
   std::ifstream input_file("channel.inp");
   grid_in.read_ucd(input_file);
 
-  FESystem<2> state_fe(FE_Q<2>(2), 2, FE_Q<2>(1), 1); //Q1
+  FESystem<2> state_fe(FE_Q<2>(2), 2, FE_Q<2>(1), 1); //Q2Q1
+
   /******hp******************/
-//  hp::FECollection<2>  state_fe_collection(state_fe);
+//  FESystem<2> state_fe_2(FE_Q<2>(3), 2, FE_Q<2>(2), 1); //Q3Q2
+//  hp::FECollection < 2 > state_fe_collection(state_fe);
+//  state_fe_collection.push_back(state_fe_2);
   /******hp******************/
 
   QGauss<2> quadrature_formula(3);
   QGauss<1> face_quadrature_formula(3);
+
   IDC idc(quadrature_formula, face_quadrature_formula);
-
   /******hp******************/
+//  QGauss<2> quadrature_formula_2(4);
+//  QGauss<1> face_quadrature_formula_2(4);
 //  hp::QCollection<2> q_coll(quadrature_formula);
+//  q_coll.push_back(quadrature_formula_2);
 //  hp::QCollection<1> face_q_coll(face_quadrature_formula);
+//  face_q_coll.push_back(face_quadrature_formula_2);
+//  IDC idc(q_coll, face_q_coll);
   /******hp******************/
 
-  LocalPDE<VECTOR, 2> LPDE;
+  LocalPDE<DOFHANDLER, VECTOR, 2> LPDE;
 
-  LocalPointFunctionalX<VECTOR, 2> LPFX;
-  LocalBoundaryFluxFunctional<VECTOR, 2> LBFF;
+  LocalPointFunctionalX<DOFHANDLER, VECTOR, 2> LPFX;
+  LocalBoundaryFluxFunctional<DOFHANDLER, VECTOR, 2> LBFF;
 
-//  //pseudo time
-//  Triangulation<1> times;
-//  GridGenerator::hyper_cube(times);
   triangulation.refine_global(3);
 
-  MethodOfLines_StateSpaceTimeHandler<FE, DOFHANDLER, SPARSITYPATTERN, VECTOR, 2> DOFH(
-      triangulation, state_fe);
-
+  MethodOfLines_StateSpaceTimeHandler<FE, DOFHANDLER, SPARSITYPATTERN, VECTOR, 2> DOFH(triangulation,state_fe);
   /***************hp********************/
-//  MethodOfLines_StateSpaceTimeHandler<2> DOFH(triangulation,state_fe_collection,times);
+//  ActiveFEIndexSetter<2> indexsetter(pr);
+//  MethodOfLines_StateSpaceTimeHandler<FE, DOFHANDLER, SPARSITYPATTERN, VECTOR, 2> DOFH(
+//      triangulation, state_fe_collection, indexsetter);
   /***************hp********************/
 
   OP P(LPDE, DOFH);
@@ -157,7 +171,7 @@ main(int argc, char **argv)
   comp_mask[1] = true;
   comp_mask[2] = false;
 
-  DOpEWrapper::ZeroFunction<2> zf(3);
+  DOpEWrapper::ZeroFunction < 2 > zf(3);
   SimpleDirichletData<VECTOR, 2> DD1(zf);
 
   BoundaryParabel boundary_parabel;
@@ -172,11 +186,6 @@ main(int argc, char **argv)
 
   SSolver solver(&P, "fullmem", pr, idc);
 
-  /***************hp********************/
-//  SSolver solver(&P,"fullmem",pr,
-//                 q_coll,
-//                 face_q_coll);
-  /***************hp********************/
   //Only needed for pure PDE Problems
   DOpEOutputHandler<VECTOR> out(&solver, pr);
   DOpEExceptionHandler<VECTOR> ex(&out);
@@ -186,28 +195,28 @@ main(int argc, char **argv)
   solver.RegisterExceptionHandler(&ex);
 
   try
-    {
-      solver.ReInit();
-      out.ReInit();
-      stringstream outp;
+  {
+    solver.ReInit();
+    out.ReInit();
+    stringstream outp;
 
-      outp << "**************************************************\n";
-      outp << "*             Starting Forward Solve             *\n";
-      outp << "*   Solving : " << P.GetName() << "\t*\n";
-      outp << "*   SDoFs   : ";
-      solver.StateSizeInfo(outp);
-      outp << "**************************************************";
-      out.Write(outp, 1, 1, 1);
+    outp << "**************************************************\n";
+    outp << "*             Starting Forward Solve             *\n";
+    outp << "*   Solving : " << P.GetName() << "\t*\n";
+    outp << "*   SDoFs   : ";
+    solver.StateSizeInfo(outp);
+    outp << "**************************************************";
+    out.Write(outp, 1, 1, 1);
 
-      solver.ComputeReducedFunctionals();
-    }
+    solver.ComputeReducedFunctionals();
+  }
   catch (DOpEException &e)
-    {
-      std::cout
-          << "Warning: During execution of `" + e.GetThrowingInstance()
-              + "` the following Problem occurred!" << std::endl;
-      std::cout << e.GetErrorMessage() << std::endl;
-    }
+  {
+    std::cout
+        << "Warning: During execution of `" + e.GetThrowingInstance()
+            + "` the following Problem occurred!" << std::endl;
+    std::cout << e.GetErrorMessage() << std::endl;
+  }
 
   return 0;
 }
