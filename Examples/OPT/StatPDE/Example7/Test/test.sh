1abe06f44c2e56341f0d80c2b5f8d5623656bf08
Winnifried Wollner <wollner@mathematik.tu-darmstadt.de>
2013-12-10 14:31:50 +0000

    Finished exchange of the examples

--- Examples/OPT/StatPDE/Example7/Test/test.sh
+++ Examples/OPT/StatPDE/Example7/Test/test.sh
@@ -14,8 +14,8 @@ if [ $1 == "Test" ]
 then
     if [ -f test.dlog ]
     then
-	echo "Running Program ../../../../../bin/DOpE-OPT-StatPDE-Example6-2d-2d test.prm"
-	(../../../../../bin/DOpE-OPT-StatPDE-Example6-2d-2d test.prm 2>&1) > /dev/null
+	echo "Running Program ../../../../../bin/DOpE-OPT-StatPDE-Example7-2d-2d test.prm"
+	(../../../../../bin/DOpE-OPT-StatPDE-Example7-2d-2d test.prm 2>&1) > /dev/null
 	echo "Comparing Results:"
 	(diff dope.log test.dlog 2>&1) > /dev/null
 	if [ $? -eq 0 ]
@@ -37,8 +37,8 @@ then
 else
     if [ $1 == "Store" ]
     then
-	echo "Running Program ../../../../../bin/DOpE-OPT-StatPDE-Example6-2d-2d test.prm"
-	(../../../../../bin/DOpE-OPT-StatPDE-Example6-2d-2d test.prm 2>&1) > /dev/null
+	echo "Running Program ../../../../../bin/DOpE-OPT-StatPDE-Example7-2d-2d test.prm"
+	(../../../../../bin/DOpE-OPT-StatPDE-Example7-2d-2d test.prm 2>&1) > /dev/null
 	echo "Run completed. Cleaning up ..."
 	mv dope.log test.dlog
 	rm -r Mesh0/
