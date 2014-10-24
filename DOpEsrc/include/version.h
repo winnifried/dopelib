269ad324ebf5328b247c94484b67c6757a7edd1a
Winnifried Wollner <wollner@mathematik.tu-darmstadt.de>
2014-10-24 17:25:42 +0000

    Updated Version to 2.0

--- DOpEsrc/include/version.h
+++ DOpEsrc/include/version.h
@@ -7,18 +7,18 @@ namespace DOpE
   {
     //Don't increase unless some major changes have happend
     //Then set minor and fix to zero
-    const unsigned int major         = 1;
+    const unsigned int major         = 2;
     //Update when you add new functionality
     //Then set fix to zero
     const unsigned int minor         = 0;
     //Update when you have fixed a bug
     const unsigned int fix           = 0;
     //If we want we can give additional information here 
-    const std::string  postfix       = "alpha";
+    const std::string  postfix       = "";
     //When updating, please increase the time
-    const unsigned int day           = 6;
-    const unsigned int month         = 12;
-    const unsigned int year          = 2013;
+    const unsigned int day           = 24;
+    const unsigned int month         = 10;
+    const unsigned int year          = 2014;
   }
 }
 
