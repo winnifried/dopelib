082e01884bf20687da711f58dbd0652a1c72d996
Thomas Wick <wick@linux-e1hk.site>
2017-04-12 15:05:22 +0200

    compressed

--- Examples/OPT/StatPDE/Example7/voidreducedproblem.h
+++ Examples/OPT/StatPDE/Example7/voidreducedproblem.h
@@ -54,7 +54,7 @@
 #include <deal.II/numerics/vector_tools.h>
 #include <deal.II/base/function.h>
 #include <deal.II/lac/sparse_matrix.h>
-#include <deal.II/lac/compressed_simple_sparsity_pattern.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/block_sparsity_pattern.h>
 #include <deal.II/lac/sparse_direct.h>
 
