83f24a740421afc353228d155438d316f79e7589
jodlbauer <jdschule@gmx.at>
2018-04-26 17:59:24 +0200

    remove #pragma once

--- DOpEsrc/include/parallel_vectors.h
+++ DOpEsrc/include/parallel_vectors.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef DOPE_PARALLEL_VECTORS_H_
+#define DOPE_PARALLEL_VECTORS_H_
 
 #ifdef DOPELIB_WITH_TRILINOS
 #include <deal.II/lac/trilinos_vector.h>
@@ -8,3 +9,5 @@
 // These work without MPI as well
 #include <deal.II/lac/parallel_vector.h>
 #include <deal.II/lac/parallel_block_vector.h>
+
+#endif
