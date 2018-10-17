b9b340baa0646dbb73534ce16d100a8dbf4a3a79
Winnifried Wollner <wollner@mathematik.tu-darmstadt.de>
2018-10-17 16:37:45 +0200

    Added skript to report runtimes of the tests

---
+++ Examples/testtime.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+failed=0
+
+for bd in OPT PDE
+do echo "Trying "$bd 
+    if [ -d $bd ]
+    then
+	cd $bd
+	for sd in StatPDE InstatPDE
+	do echo "Trying "$bd"/"$sd
+	    if [ -d $sd ]
+	    then
+		cd $sd
+		for i in `ls -d Example*`
+		do cd $i
+		    echo -n -e "In "$bd"/"$sd"/"$i" \t Test: " 
+		    if [ -d Test ]
+		    then
+			cd Test
+			if [ -f test.sh ]
+			then
+			    ((time (./test.sh Test 2>&1 ) > /dev/null ) 2>&1 )> time.txt
+			    if [ $? -eq 0 ]
+			    then
+				echo -en '\E[32;40m'"succeeded!"
+				tput sgr0
+				echo -n " "
+				sed 's/s$/s\t/g' time.txt | sed 's/real\t/r: /g' | sed 's/user\t/u: /g' | sed 's/sys\t/s: /g' | tr -d '\n'
+				echo
+			    else
+				echo -en '\E[31;40m'"   failed!"
+				tput sgr0
+				sed 's/s$/s\t/g' time.txt | tr -d '\n'
+				echo
+				failed=$((${failed} + 1))
+			    fi
+			    rm time.txt
+			else
+			    echo -en '\E[31;40m'"   failed!"
+			    tput sgr0
+			    echo 
+			    failed=$((${failed} + 1))
+			fi
+			cd ..
+		    else
+			echo -en "'\E[31;40m'   failed!"
+			tput sgr0
+			echo
+			failed=$((${failed} + 1))
+		    fi
+		    cd ..
+		done
+		cd ..
+	    fi
+	    wait
+	done
+	cd ..
+    fi
+done
+exit $failed
