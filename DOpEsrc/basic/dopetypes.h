88a8c19b8021b23f5043c244c16d34785c19a030
Daniel Jodlbauer <daniel.jodlbauer@dk-compmath.jku.at>
2018-10-17 21:41:12 +0200

    mpi dependent debug info

--- DOpEsrc/basic/dopetypes.h
+++ DOpEsrc/basic/dopetypes.h
@@ -30,8 +30,11 @@
 
 #include <deal.II/base/utilities.h>
 
-// TODO remove
+#ifdef DEAL_II_WITH_MPI
 #define MPI_TILL_HERE printf("[%d] Line %d \t File %s \n", dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD), __LINE__, __FILE__)
+#else
+#define MPI_TILL_HERE printf("Line %d \t File %s \n", __LINE__, __FILE__)
+#endif
 
 namespace DOpE
 {
