e76bcb15b92cf6bde1b166605f42857c7920d2a1
bbb4232 <christine.herter@uni-hamburg.de>
2023-12-13 10:23:50 +0100

    added test EV_Maxwell_rect

--- Examples/EV/Example_Maxwell_Rect/main.cc
+++ Examples/EV/Example_Maxwell_Rect/main.cc
@@ -81,6 +81,7 @@
 #include "localfunctional.h"
 #include "deformation_functions.h"
 #include <basic/test_mol_spacetimehandler.h>
+#include "functionals.h"
 
 using namespace std;
 using namespace dealii;
@@ -192,6 +193,10 @@ main(int argc, char **argv){
 
   NoConstraints<CDC, FDC, DOFHANDLER, VECTOR, CDIM, DIM> Constraints;
   OP P(LFunc, LPDE, Constraints, DOFH);
+  
+    //Functional which returns acutal eigenvalue
+  LocalEigenvalueFunctional<CDC, FDC, DOFHANDLER, VECTOR, CDIM, DIM> EVF;
+  P.AddFunctional(&EVF);
 
   std::vector<bool> comp_mask(3);
 
